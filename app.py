2cb87f14e5029a760a6ec2a04c8383aafcb2f1ff
JosephMugo <joemu18@gmail.com>
2020-11-21 14:11:42 -0600

    instantiated flask migrate

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-# db.create_all()
+
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
 #----------------------------------------------------------------------------#
