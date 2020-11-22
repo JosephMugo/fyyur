3ac2c887fd854517f00b7f01aef15898ac2c0a93
JosephMugo <joemu18@gmail.com>
2020-11-22 15:28:49 -0600

    modeled database

--- app.py
+++ app.py
@@ -33,35 +33,73 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
+venue_genre = db.Table('venue_genre',
+    db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True)
+)
+
+artist_genre = db.Table('artist_genre',
+    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True)
+)
+
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    venues = db.relationship('Venue', secondary=venue_genre, backref=db.backref('genres', lazy='dynamic'))
+    artists = db.relationship('Artist', secondary=artist_genre, backref=db.backref('genres', lazy='dynamic'))
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+    start_time = db.Column(db.DateTime)
+    
+    artist = db.relationship('Artist', back_populates="venues")
+    venue = db.relationship('Venue', backref=db.backref('artists'))
+
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False) 
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=False)
+    # new 
+    website = db.Column(db.String(120), nullable=False)
+    seeking_talent = db.Column(db.Boolean, nullable=False)
+    seeking_description = db.Column(db.String, nullable=False)
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    artist = db.relationship('Show', back_populates='venue')
+
+    
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    # genres = db.Column(db.String(120)) || removed (will create table for this) ||
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=False)
+    # new 
+    website = db.Column(db.String, nullable=False)
+    seeking_venue = db.Column(db.Boolean, nullable=False)
+    seeking_description = db.Column(db.String, nullable=False)
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
+    venues = db.relationship('Show', back_populates='artist')
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    
 
 #----------------------------------------------------------------------------#
 # Filters.
